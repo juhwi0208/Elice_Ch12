efd3e0258bcf29b88e5f55b561a078d7a32f05d0
juhwi <juhwi0208@gmail.com>
2026-08-05 14:13:24 +0900

    develop branch third commit

--- branch_test.py
+++ branch_test.py
@@ -1,3 +1,4 @@
 print("new branch test")
 print("develop branch publish")
-print("develop branch publish 2")
+print("develop branch commit 2")
+print("develop branch commit 3 after create feature branch")
